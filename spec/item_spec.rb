e909361212a74a6d5e98e866e127e32a3be87047
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-12 21:29:02 +0000

    Spec and implementation for an item

---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require_relative '../lib/item'
+
+RSpec.describe Item do
+  subject(:item) { described_class.new(name: name, price: price, quantity: quantity) }
+  let(:name) { 'Name' }
+  let(:price) { 40 }
+  let(:quantity) { 5 }
+
+  it 'returns a name' do expect(item.name).to eq(name) end
+  it 'returns a price' do expect(item.price).to eq(price) end
+  it 'returns a quantity' do expect(item.quantity).to eq(quantity) end
+end
