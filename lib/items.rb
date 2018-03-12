4563c9c8139b7aa2de655b6d89ddba3a0889a935
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-12 23:00:20 +0000

    Price of item

--- lib/items.rb
+++ lib/items.rb
@@ -5,6 +5,10 @@ class Items
     @stock = stock
   end
 
+  def price_of_item(name_of_item)
+    find_item(name_of_item).price
+  end
+
   def find_item(name_of_item)
     item = stock.detect{|item| item.name == name_of_item }
     raise 'Item could not be found' if item.nil?
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Items do
     Item.new(name: 'Foo', price: 40, quantity: 5),
     Item.new(name: 'Bar', price: 30, quantity: 8)
   ] }
+  let(:item) { items.stock.sample }
 
   it 'returns a stock' do expect(items.stock).to eq(stock) end
 
-  describe '#find item' do
+  describe '#find_item' do
     subject(:find_item) { items.find_item(item.name) }
 
     context 'when the item exists' do
-      let(:item) { items.stock.sample }
       it 'returns the item' do
         expect(find_item).to eq item
       end
@@ -24,6 +24,14 @@ RSpec.describe Items do
       it 'returns the item' do
         expect{ find_item }.to raise_error 'Item could not be found'
       end
+    end
+  end
+
+  describe '#price_of_item' do
+    subject(:price_of_item) { items.price_of_item(item.name) }
+
+    it 'returns the price of an item' do
+      expect(price_of_item).to eq item.price
     end
   end
 end
