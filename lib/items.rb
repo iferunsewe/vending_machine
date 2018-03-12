1e284bbfca708a8d6b338ed921e8282b7c855e32
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-12 22:20:21 +0000

    Made items class for collection of items

---
+++ lib/items.rb
@@ -0,0 +1,9 @@
+class Items
+  attr_reader :stock
+
+  def initialize(stock:)
+    @stock = stock
+  end
+
+
+end
--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'items'
+
 class VendingMachine
   attr_reader :float, :items
 
