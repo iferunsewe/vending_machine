bf487ece3c8a0bbe9711f55829a02a02f9e25b2c
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-13 13:20:19 +0000

    Changed it to required file

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,23 +1,5 @@
+require_relative 'item'
 require_relative 'items'
-
-class VendingMachine
-  attr_reader :float, :items
-
-  def initialize
-    @items = []
-    @float = nil
-  end
-
-  def load_items(items)
-    @items = Items.new(stock: items)
-  end
-
-  def load_float(money_options)
-    @float = MoneyCollection.new(money_options)
-  end
-
-  def buy(name_of_item:, customer_purse:)
-    @float + customer_purse
-    @items.reduce_item(name_of_item)
-  end
-end
+require_relative 'money_collection'
+require_relative 'transaction'
+require_relative 'machine'
