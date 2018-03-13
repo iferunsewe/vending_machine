d856171e87357260d81a6f5f9a0ed8b4d4413d1a
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-13 12:04:18 +0000

    change and amount still required for transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,12 @@
 class Transaction
+  attr_reader :amount_still_required, :change
+
   def initialize(item:, customer_cash_total:)
     @item = item
     @customer_cash_total = customer_cash_total
     @amount_required = @item.price
+    @amount_still_required = amount_still_required
+    @change = change
   end
 
   def sufficient_funds?
@@ -11,5 +15,15 @@ class Transaction
 
   def insufficient_funds?
     !sufficient_funds?
+  end
+
+  def amount_still_required
+    return 0 if sufficient_funds?
+    @amount_required - @customer_cash_total
+  end
+
+  def change
+    return 0 if @customer_cash_total == @amount_required || insufficient_funds?
+    @customer_cash_total - @amount_required
   end
 end
--- spec/transanction_spec.rb
+++ spec/transanction_spec.rb
@@ -54,4 +54,54 @@ RSpec.describe Transaction do
       end
     end
   end
+
+  describe '#amount_still_required' do
+    subject(:amount_still_required) { transaction.amount_still_required }
+
+    context 'when there is more customer cash than the price of the item' do
+      let(:customer_cash_total) { 50 }
+      it 'returns 0' do
+        expect(amount_still_required).to eq 0
+      end
+    end
+
+    context 'when the customer cash and the price of the item are equal' do
+      let(:customer_cash_total) { 40 }
+      it 'returns 0' do
+        expect(amount_still_required).to eq 0
+      end
+    end
+
+    context 'when there is less customer cash than the price of the item' do
+      let(:customer_cash_total) { 30 }
+      it 'returns the items price minus the customer cash' do
+        expect(amount_still_required).to eq item.price - customer_cash_total
+      end
+    end
+  end
+
+  describe '#change' do
+    subject(:change) { transaction.change }
+
+    context 'when there is more customer cash than the price of the item' do
+      let(:customer_cash_total) { 50 }
+      it 'returns 0' do
+        expect(change).to eq customer_cash_total - item.price
+      end
+    end
+
+    context 'when the customer cash and the price of the item are equal' do
+      let(:customer_cash_total) { 40 }
+      it 'returns 0' do
+        expect(change).to eq 0
+      end
+    end
+
+    context 'when there is less customer cash than the price of the item' do
+      let(:customer_cash_total) { 30 }
+      it 'returns the items price minus the customer cash' do
+        expect(change).to eq 0
+      end
+    end
+  end
 end
