1149532799ff5f095e58fe899050524e0e81f255
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-13 00:14:58 +0000

    Load float

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe VendingMachine do
       expect(vending_machine.items.stock).to eq items
     end
   end
+
+  describe '#load_float' do
+    subject(:load_float) { vending_machine.load_float(money_options) }
+    let(:money_options){ {
+      '5p' => '10',
+      '20p' => '3',
+      '50p' => '4',
+      '£1' => '4',
+    } }
+    it 'sets the float attribute in the vending_machine' do
+      expect{ load_float }.to change{ vending_machine.float }.from(nil).to(MoneyCollection)
+    end
+  end
 end
