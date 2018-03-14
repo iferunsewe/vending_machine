7076dd980c7a6e82d9a1eb7bcd7b85b0c2cc2f61
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-14 13:28:45 +0000

    made create stock private method

--- lib/items.rb
+++ lib/items.rb
@@ -2,7 +2,7 @@ class Items
   attr_reader :stock
 
   def initialize(stock:)
-    @stock = stock
+    @stock = create_stock(stock)
   end
 
   def price_of_item(name)
@@ -25,5 +25,13 @@ class Items
     item = find_item(name)
     item.quantity -= 1
     item
+  end
+
+  private
+
+  def create_stock(items)
+    items.map do |item|
+      Item.new(name: item[:name], price: item[:price], quantity: item[:quantity])
+    end
   end
 end
