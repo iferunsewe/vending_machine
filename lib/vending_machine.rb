86063e6c955e4282aca431d29fe1b78970832e3d
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-15 16:13:15 +0000

    Changed load order

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'pry'
+require 'highline/import'
+require 'highline'
+require 'money'
 require_relative 'item'
 require_relative 'items'
 require_relative 'money_collection'
 require_relative 'transaction'
 require_relative 'machine'
-require 'highline/import'
-require 'highline'
-require 'money'
-require 'terminal-table'
 I18n.enforce_available_locales = false
 require_relative 'presenter'
 
